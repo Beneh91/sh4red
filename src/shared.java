c00a383873845db3d266d0c9d15cc83b6b980a0a
Beneh91 <abeneharo@gmail.com>
2016-03-31 17:45:49 +0100

    revision push

---
+++ src/shared.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+
+public class shared {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi world");
+JOptionPane.showConfirmDialog(null, "bye");
+JOptionPane.showInputDialog("algo que comentar");
+	} 
+
+}
