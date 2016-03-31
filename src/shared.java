d247408caa0fcc11f7b99387b8a26230fc7c0a05
Beneh91 <abeneharo@gmail.com>
2016-03-31 19:44:12 +0100

    Update shared.java

--- src/shared.java
+++ src/shared.java
@@ -5,7 +5,7 @@ public class shared {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi world");
+System.out.println("hiworld");
 JOptionPane.showConfirmDialog(null, "bye");
 JOptionPane.showInputDialog("algo que comentar");
 JOptionPane.showMessageDialog(null, "vale, todo ok");
